924354859752daf636d41751aa3dbbfd385eb9a7
vipsai1995 <vipsai1995@gmail.com>
2025-08-04 13:12:47 +0530

    udpate on test runner

--- src/test/java/runnerPackage/TestRunner.java
+++ src/test/java/runnerPackage/TestRunner.java
@@ -14,31 +14,61 @@ public class TestRunner {
     
 //    @Karate.Test
 //    Karate getResponseApi() {
-//		return Karate.run(featureFile+"getResponseApi.feature");						// works if the .feature file exist in different package
+//		return Karate.run(featureFile+"getResponseApi.feature");						
 //    }
     
 //    @Karate.Test
 //    Karate getDataFromConfigApi() {
-//		return Karate.run(featureFile+"scenario.feature");						// works if the .feature file exist in different package
+//		return Karate.run(featureFile+"scenario.feature");						
 //    }
     
 //    @Karate.Test
 //    Karate queryApi() {
-//		return Karate.run(featureFile+"queryApi.feature");						// works if the .feature file exist in different package
+//		return Karate.run(featureFile+"queryApi.feature");						
 //    }
     
 //    @Karate.Test
 //    Karate requestHeadersApi() {
-//		return Karate.run(featureFile+"requestHeaders.feature");						// works if the .feature file exist in different package
+//		return Karate.run(featureFile+"requestHeaders.feature");						
 //    }
     
-    @Karate.Test
-    Karate postapi() {
-		return Karate.run(featureFile+"post.feature");						// works if the .feature file exist in different package
-    }
+//    @Karate.Test
+//    Karate postapi() {
+//		return Karate.run(featureFile+"post.feature");						
+//    }
     
 //    @Karate.Test
 //    Karate verifyTheResponse() {
-//		return Karate.run(featureFile+"verifyTheResponse.feature");						// works if the .feature file exist in different package
+//		return Karate.run(featureFile+"verifyTheResponse.feature");						
 //    }
+	
+//	  @Karate.Test
+//	  Karate putapi() {
+//			return Karate.run(featureFile+"put.feature");						
+//	  }	
+	
+//	  @Karate.Test
+//	  Karate variable() {
+//			return Karate.run(featureFile+"variable.feature");						
+//	  }
+	
+//	  @Karate.Test
+//	  Karate deleteapi() {
+//			return Karate.run(featureFile+"delete.feature");						
+//	  }	
+	  
+//	  @Karate.Test
+//	  Karate readExternalFileapi() {
+//			return Karate.run(featureFile+"ReadExternalFile.feature");						
+//	  }	
+	  
+//	  @Karate.Test
+//	  Karate CreateNestedJsonObjectapi() {
+//			return Karate.run(featureFile+"CreateNestedJsonObject.feature");						
+//	  }	
+	  
+	  @Karate.Test
+	  Karate generateRandomValuesUsingDataFakerapi() {
+			return Karate.run(featureFile+"GenerateRandomValuesforPayloadUsingDataFaker.feature");						
+	  }	
 }
